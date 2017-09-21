1de2f3fc027568b13e5317147ae57751c00d7c7f
superspeeder <andy@delusion.org>
2017-09-21 10:19:44 -0700

    Update distance.py

--- metric/distance.py
+++ metric/distance.py
@@ -1,2 +1,27 @@
 def meters_to_kilometers(meters):
   return meters/1000
+
+def meters_to_hectometers(meters):
+  return meters/100
+
+def meters_to_dekameters(meters):
+  return meters/10
+
+def meters_to_decimeters(meters):
+  return meters*10
+
+def meters_to_centimeters(meters):
+  return meters*100
+
+def meters_to_millimeters(meters):
+  return meters*1000
+
+def meters_to_micrometers(meters):
+  return meters*1000000
+
+def inches_to_feet(inches):
+  return inches/12
+
+def inches_to_yards(inches):
+  return inches/36
+
